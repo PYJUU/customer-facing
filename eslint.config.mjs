d1db479277ce1dc42e149efce6bc4b3394d692f9
PYJU <129842285+PYJUU@users.noreply.github.com>
2026-04-10 09:59:46 +0800

    docs: add MVP requirements checklist and enforce pre-commit quality gates

--- eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
-
-const config = [...nextVitals, ...nextTs];
-
-export default config;
